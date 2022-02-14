eaa6f1263d28a28e30e34346b7eb361b9e85a065
ecvunge <edvaldocv@ext.inditex.com>
2022-02-14 22:52:27 +0000

    criação do pacote ageByYearsExercises e a classe ageByYears

---
+++ SalaryAlgorithms/src/ageByYearsExercises/ageByYears.java
@@ -0,0 +1,34 @@
+package ageByYearsExercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ageByYears {
+
+	public static void main(String [] args) {
+		/*
+		 Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+		mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+		(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
+		*/
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		 
+		System.out.println("digite a sua idade apenas em anos:?");
+		double years = sc.nextInt();
+		
+		System.out.println("digite a sua idade apenas em meses:?");
+		double moths = sc.nextInt();
+		
+		System.out.println("digite a sua idade apenas em dias:?");
+		double days = sc.nextInt();
+		
+		days =( days + ( years * 365) + (moths * 30));
+		
+		System.out.println( " a sua idade em dias é: " + days );
+		
+		sc.close();
+		
+	}
+}
