0c5f0c0d18e24de2d49a86287b45e9d8e6493038
ecvunge <edvaldocv@ext.inditex.com>
2022-02-15 12:49:26 +0000

    criação da classe create Vetor Exercises

--- SalaryAlgorithms/src/createVetorExercises/CreateVetor.java
+++ SalaryAlgorithms/src/createVetorExercises/CreateVetor.java
@@ -2,4 +2,13 @@ package createVetorExercises;
 
 public class CreateVetor {
 
+	/*	
+	 	Crie dois vetores de 50 posições com valores inteiros aleatórios, ordene cada vetor
+		individualmente, e combine os dois vetores gerando um novo vetor de 100 posições, de
+		forma que esse novo vetor já seja criado ordenado
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
 }
